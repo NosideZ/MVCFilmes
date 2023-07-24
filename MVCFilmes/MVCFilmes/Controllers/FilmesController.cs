47416e590bd3678ba3272316e9082a30fef41f13
Edson Zimpel <edisonzimpel@gmail.com>
2023-07-24 09:47:57 -0300

    Arquivo Inicial MVCFilmes

---
+++ MVCFilmes/MVCFilmes/Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCFilmes.Controllers
+{
+    public class FilmesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BemVindo(string nome, int id)
+        {
+            ViewData["Title"] = "Bem Vindo!";
+            ViewData["Nome"] = nome;
+            ViewData["NumeroX"] = id;
+            return View(); 
+        }
+    }
+}
---
+++ MVCFilmes/MVCFilmes/Models/Filmes.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace MVCFilmes.Models
+{
+    public class Filmes
+    {
+        public int ID { get; set; }
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Este campo é obrigatório"), MaxLength(100)]
+        public string Titulo { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Lançamento")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public DateTime DataLancamento { get; set; }
+        [DisplayName("Gênero")]
+        [Required(ErrorMessage = "Este campo é obrigatório"), StringLength(10), RegularExpression(@"^[A-Z]+[a-zA-z\s]*$", ErrorMessage = "Dados Invalidos")]
+        public string Genero { get; set; } = string.Empty;
+        [DisplayName("Preço")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+   //     [DisplayFormat(DataFormatString = "{0:0,0.00}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public decimal Preco { get; set; }
+        [Range(0,5, ErrorMessage ="Somente Numeros de 0 a 5")]
+        public int? Pontos { get; set; }
+    }
+}
